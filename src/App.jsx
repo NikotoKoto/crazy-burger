12e31c220eaa5f015e3786430a82c6f98c05ba55
NikotoKoto <floris.nicolas77@gmail.com>
2024-05-22 14:17:19 +0200

    ✨ F01 : LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
+import LoginPage from './components/pages/loginPage'
 
 function App() {
-  // state (etat, données)
-
-
-
-  // comportements
-const handleCo = () => {
-  alert("bonjour Bob")
-
-}
 
   //render
   return (
-  <div>
-    <h1>Bienvenue chez nous</h1>
-    <h2>Connectez-vous!</h2>
-    <form>
-      <input placeholder='Entrez votre prénom...' required></input>
-      <button onClick={handleCo}>Acceder à votre espace</button>
-    </form>
-  </div>)
+ <LoginPage/>)
 }
 
 export default App
