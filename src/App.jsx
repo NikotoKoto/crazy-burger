914b2b77c3bd1ccdd2ee0b22049f5811dd7465b4
NikotoKoto <floris.nicolas77@gmail.com>
2024-05-22 14:08:37 +0200

    🔥 F01: Delete app.test.js(Apprendre à commit)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // state (etat, données)
 
+
+
+  // comportements
+const handleCo = () => {
+  alert("bonjour Bob")
+
+}
+
+  //render
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-         Bonsoir tout le monde
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div>
+    <h1>Bienvenue chez nous</h1>
+    <h2>Connectez-vous!</h2>
+    <form>
+      <input placeholder='Entrez votre prénom...' required></input>
+      <button onClick={handleCo}>Acceder à votre espace</button>
+    </form>
+  </div>)
 }
 
 export default App
