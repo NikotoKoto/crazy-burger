348df2faa8dee3d6e5d48ff7862b77d1a147d1f8
NikotoKoto <floris.nicolas77@gmail.com>
2024-05-23 09:22:02 +0200

    ✏️ F02: Fixe typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import LoginPage from './components/pages/login/loginPage'
 
 function App() {
+  //state
+
+  //comportements
 
   //render
   return (
---
+++ src/components/pages/order/orderPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function orderPage() {
+    //state
+
+    //comportement
+
+    //render
+  return (
+    <div>
+        <h1>Bienvenue toto</h1>
+        <button>Deconnexion</button>
+    </div>
+  )
+}
