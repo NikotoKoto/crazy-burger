52faeebb12b29b22a1ede6120a110b62a0b9aad8
NikotoKoto <floris.nicolas77@gmail.com>
2024-05-27 16:13:03 +0200

    ♻️ F03 : LoginPage > Refactor

--- src/components/pages/login/loginPage.jsx
+++ src/components/pages/login/loginPage.jsx
@@ -2,43 +2,36 @@ import LoginForm from "./loginForm";
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import Logo from "../../reusable-ui/Logo";
-import background from "../../../../public/burger-background.jpg"
+import background from "../../../../public/burger-background.jpg";
 
 export default function loginPage() {
-
-  
   //render
   return (
     <LoginPageStyled>
-      <Logo/>
-      <LoginForm/>
+      <Logo />
+      <LoginForm />
     </LoginPageStyled>
-
   );
-
-
 }
 
 const LoginPageStyled = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-height: 100vh;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-
-
-
-::before {
-  content: "";
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background});
-  background-size: cover;
-  background-position: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: -1;
-}
-`
+  ::before {
+    content: "";
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url(${background});
+    background-size: cover;
+    background-position: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
